f6bafe072e4794954bf652ce6fdffed940ca8191
Abhishek bhagat <abhishekbhagatraw@gmail.com>
2020-11-11 19:42:19 +0530

    [abhishek] add: read a number and display the week day

---
+++ weekday.js
@@ -0,0 +1,15 @@
+var number=process.argv[2];
+if(number==0)
+ console.log("Sunday");
+else if(number==1)
+console.log("Monday");
+else if(number==2)
+console.log("Tuesday");
+else if(number==3)
+console.log("Wednesday");
+else if(number==4)
+console.log("Thrusday");
+else if(number==5)
+console.log("Friday")
+else if(number==6)
+console.log("Saturday");
