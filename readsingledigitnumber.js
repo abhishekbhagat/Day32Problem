3b558bd139e317e3454df109f3e7275969163a26
Abhishek bhagat <abhishekbhagatraw@gmail.com>
2020-11-11 19:41:11 +0530

    [abhishek] add: read a single digit number and write number in word

---
+++ readsingledigitnumber.js
@@ -0,0 +1,21 @@
+var number=process.argv[2];
+if(number==0)
+ console.log("Zero");
+else if(number==1)
+console.log("One");
+else if(number==2)
+console.log("two");
+else if(number==3)
+console.log("Three");
+else if(number==4)
+console.log("four");
+else if(number==5)
+console.log("five")
+else if(number==6)
+console.log("six");
+else if(number==7)
+console.log("seven");
+else if(number==8)
+console.log("eight")
+else if(number==9)
+console.log("nine");
