af3c62c126891d41cb40f8ee4dec0b50a5e42851
Abhishek bhagat <abhishekbhagatraw@gmail.com>
2020-11-11 19:59:26 +0530

    [abhishek] add: read a number 1,10,100 and display unit,ten

---
+++ numberinwordusingcase.js
@@ -0,0 +1,24 @@
+var num = process.argv[2];
+var number=parseInt(num);
+switch (number) {
+    case 1:
+        console.log("One");
+        break;
+    case 10:
+        console.log("Ten");
+        break;
+    case 100:
+        console.log("Hundred");
+        break;
+    case 1000:
+        console.log("Thousand");
+        break;
+    case 10000:
+        console.log("Ten Thousand");
+        break;
+    case 100000:
+        console.log("Lakh")
+        break;
+    default:
+        console.log("please enter valid  number ");
+}
