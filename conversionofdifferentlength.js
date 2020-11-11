54f69166fc0f55038edffa8d13e4559613352569
Abhishek bhagat <abhishekbhagatraw@gmail.com>
2020-11-11 20:00:54 +0530

    [abhishek] add: a program that takes user input and does unit conversion

---
+++ conversionofdifferentlength.js
@@ -0,0 +1,7 @@
+//var num=process.argv[2];
+var number=1;
+//parseInt(num);
+console.log(number+" Feet = " + 12*number+" inch");
+console.log(number+" Inch = " + 0.0833*number+" Feet");
+console.log(number+" Feet = " + 0.3048*number+" Meter");
+console.log(number+" Meter = " + 3.2808*number+" Feet");
